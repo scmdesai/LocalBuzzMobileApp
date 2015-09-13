859746fe8d15f6c85bd0dea5821ad50ed633a1de
scmdesai <scmdesai@gmail.com>
2015-09-13 14:16:12 -0500

    deleted sencha_backup

--- .sencha_backup/Contact/2.4.0.487/build/temp/testing/Contact/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require H:\Apps\Sencha Architect Apps\MyCouponApp\app.js
--- .sencha_backup/Contact/2.4.2.571/build/temp/testing/Contact/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require H:\Apps\Sencha Architect Apps\MyCouponApp_CustomButton\app.js
